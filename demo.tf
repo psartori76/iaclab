bc0685cbcc2711ff7649a33970abb9c92dd885de
Paulo Sartori <paulo.sartori@oracle.com>
2022-04-08 15:39:37 -0300

    second commit

--- demo.tf
+++ demo.tf
@@ -1,15 +1,15 @@
-variable "compartment_ocid" {
-    default = "ocid1.compartment.oc1..aaaaaaaawa2imxuftedpg2dknhmkqanyz7go6srvenlkjpq7oful5folwlzq"
-}
-variable "region" {
-    default = "us-ashburn-1"
-}
-variable "bucket_namespace" {
-    default = "id0cvlngwjnu"
-}
-variable "bucket_name" {
-    default = "resource_manager_demo_bucket"
-}
+variable "compartment_ocid" {}
+#    default = "ocid1.compartment.oc1..aaaaaaaawa2imxuftedpg2dknhmkqanyz7go6srvenlkjpq7oful5folwlzq"
+#}
+variable "region" {}
+#    default = "us-ashburn-1"
+#}
+#variable "bucket_namespace" {
+#    default = "id0cvlngwjnu"
+#}
+variable "bucket_name" {}
+#    default = "resource_manager_demo_bucket"
+#}
 
 provider "oci" {
   region = var.region
